70156a897452d242919132aac0f0961254d9f2ec
Kirill Kuyan <progingisfun@gmail.com>
2019-11-19 19:57:19 +0300

    Updated WallService contract

--- src/main/java/com/project/shishafan/services/interfaces/WallService.java
+++ src/main/java/com/project/shishafan/services/interfaces/WallService.java
@@ -16,6 +16,7 @@ import java.util.List;
 public interface WallService {
 
     Integer getPostsCount(int ownerId, WallFilter wallFilter);
+    Integer getCommentsCount(int ownerId, int postId);
     List<Post> getWallPosts(int ownerId, int count, int offset, WallFilter filter);
     List<Comment> getPostComments(int ownerId, int postId, boolean likes, int offset, int count);
 }
