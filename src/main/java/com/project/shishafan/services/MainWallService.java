161e0d1818ad669e5f7447312a2d20d01603d18d
Kirill Kuyan <progingisfun@gmail.com>
2019-11-19 19:58:07 +0300

    Implemented WallService interface

--- src/main/java/com/project/shishafan/services/MainWallService.java
+++ src/main/java/com/project/shishafan/services/MainWallService.java
@@ -4,49 +4,142 @@ package com.project.shishafan.services;
  *  Date: 01.11.2019
  */
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.shishafan.drivers.models.Comment;
+import com.project.shishafan.drivers.models.Post;
 import com.project.shishafan.services.interfaces.WallService;
 import com.vk.api.sdk.actions.Wall;
+import com.vk.api.sdk.client.ClientResponse;
 import com.vk.api.sdk.client.actors.ServiceActor;
-import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.enums.WallFilter;
-import com.vk.api.sdk.objects.wall.responses.GetCommentsResponse;
-import com.vk.api.sdk.objects.wall.responses.GetResponse;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainWallService implements WallService {
 
     private Wall wall;
     private ServiceActor serviceActor;
+    private ObjectMapper mapper;
 
     public MainWallService(Wall vk, ServiceActor serviceActor) {
         this.wall = vk;
         this.serviceActor = serviceActor;
+        this.mapper = new ObjectMapper();
     }
 
-    public GetResponse getWallPosts(int ownerId, int count, int offset, WallFilter filter){
+    @Override
+    public Integer getPostsCount(int ownerId, WallFilter wallFilter) {
+
+        int postsCount = 0;
+
         try {
-            return wall.get(serviceActor)
-                    .ownerId(ownerId).count(count).offset(offset).filter(filter).execute();
-        } catch (ApiException e) {
-            e.printStackTrace();
-        } catch (ClientException e) {
+            ClientResponse clientResponse = wall.get(serviceActor).ownerId(ownerId).filter(wallFilter).executeAsRaw();
+            String content = clientResponse.getContent();
+            postsCount = mapper.readTree(content).get("response").get("count").asInt();
+        } catch (ClientException | JsonProcessingException e) {
             e.printStackTrace();
         }
 
-        return new GetResponse();
+        return postsCount;
     }
 
-    public GetCommentsResponse getPostComments(int ownerId, int postId, boolean likes, int offset, int count){
+    @Override
+    public Integer getCommentsCount(int ownerId, int postId) {
+
+        int commentsCount = 0;
+
         try {
-            return wall.getComments(serviceActor)
-                    .ownerId(ownerId).postId(postId).needLikes(likes).offset(offset).count(count).execute();
-        } catch (ApiException e) {
-            e.printStackTrace();
-        } catch (ClientException e) {
+            ClientResponse clientResponse = wall.getComments(serviceActor)
+                    .ownerId(ownerId).postId(postId).needLikes(false).offset(0).count(0).executeAsRaw();
+            String content = clientResponse.getContent();
+            commentsCount = mapper.readTree(content).get("response").get("count").asInt();
+        } catch (ClientException | JsonProcessingException e) {
             e.printStackTrace();
         }
 
-        return new GetCommentsResponse();
+        return commentsCount;
+    }
+
+
+    public List<Post> getWallPosts(int ownerId, int count, int offset, WallFilter filter) {
+
+        List<Post> posts = Collections.emptyList();
+
+        try {
+            ClientResponse clientResponse = wall.get(serviceActor)
+                    .ownerId(ownerId).count(count).offset(offset).filter(filter).executeAsRaw();
+            String content = clientResponse.getContent();
+            JsonNode items = getItemsFromJsonString(content);
+            posts = getPostsFromItemsJson(items);
+        } catch (ClientException | JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return posts;
+
+
+    }
+
+    public List<Comment> getPostComments(int ownerId, int postId, boolean likes, int offset, int count) {
+
+        List<Comment> comments = Collections.emptyList();
+
+        try {
+            ClientResponse clientResponse = wall.getComments(serviceActor)
+                    .ownerId(ownerId).postId(postId).needLikes(likes).offset(offset).count(count).executeAsRaw();
+            String content = clientResponse.getContent();
+            JsonNode items = getItemsFromJsonString(content);
+            comments = getCommentsFromItemsJson(items);
+        } catch (ClientException | JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return comments;
+    }
+
+    private JsonNode getItemsFromJsonString(String content) throws JsonProcessingException {
+        return mapper.readTree(content).get("response").get("items");
+    }
+
+    private List<Post> getPostsFromItemsJson(JsonNode items) {
+
+        List<Post> posts = new ArrayList<>();
+
+        for (JsonNode node : items) {
+            posts.add(new Post(
+                    node.get("id").asInt(),
+                    node.get("text").asText(),
+                    node.get("likes").get("count").asInt(),
+                    node.get("reposts").get("count").asInt(),
+                    node.get("comments").get("count").asInt()));
+        }
+
+        return posts;
+
+    }
+
+    private List<Comment> getCommentsFromItemsJson(JsonNode items) {
+
+        List<Comment> comments = new ArrayList<>();
+
+        for (JsonNode node : items) {
+            JsonNode textNode = node.get("text");
+            JsonNode likesNode = node.get("likes");
+            JsonNode idNode = node.get("id");
+
+            int id = idNode != null ? idNode.asInt() : -1;
+            String text = textNode != null ? textNode.asText() : "";
+            int likes = likesNode != null ? (likesNode.get("count") != null ? likesNode.get("count").asInt() : -1) : -1;
+            
+            comments.add(new Comment(id, text, likes));
+        }
+
+        return comments;
     }
 
 
