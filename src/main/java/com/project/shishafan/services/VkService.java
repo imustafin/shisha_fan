73612929735376dcd8e0c2d230003c71e87dc531
Kirill Kuyan <progingisfun@gmail.com>
2019-11-19 19:59:19 +0300

    Implemented VkService
    Added implementation of main methods

--- src/main/java/com/project/shishafan/services/VkService.java
+++ src/main/java/com/project/shishafan/services/VkService.java
@@ -4,8 +4,17 @@ package com.project.shishafan.services;
  *  Date: 07.11.2019
  */
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.shishafan.drivers.models.Comment;
+import com.project.shishafan.drivers.models.Post;
+import com.project.shishafan.services.interfaces.AuthService;
+import com.project.shishafan.services.interfaces.WallService;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.enums.WallFilter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VkService {
 
@@ -13,22 +22,61 @@ public class VkService {
     private AuthService authService;
     private WallService wallService;
 
+    private final int POSTS_FETCH_SIZE = 100;
+    private final int COMMENTS_FETCH_SIZE = 100;
+
 
     public VkService() {
-        vk = new VkApiClient(HttpTransportClient.getInstance());
-        authService = new AuthService();
-        wallService = new WallService(vk.wall(),authService.getServiceActor());
+        this.vk = new VkApiClient(HttpTransportClient.getInstance());
+        this.authService = new MainAuthService();
+        this.wallService = new MainWallService(vk.wall(), authService.getServiceActor());
     }
 
-    public VkApiClient api(){
+    public VkApiClient api() {
         return vk;
     }
 
-    public void getAllPostsFromGroup(){
+    public List<Post> getAllPostsFromGroup(int ownerId) {
 
+        int remains = wallService.getPostsCount(ownerId, WallFilter.OWNER);
+        List<Post> wallPosts = new ArrayList<>();
+
+        int iteration = 0;
+        int offset;
+        do {
+            offset = iteration * POSTS_FETCH_SIZE;
+            List<Post> subPosts = wallService.getWallPosts(ownerId, POSTS_FETCH_SIZE, offset, WallFilter.OWNER);
+            wallPosts.addAll(subPosts);
+            remains -= POSTS_FETCH_SIZE;
+            iteration++;
+        } while (remains > 0);
+
+        return wallPosts;
     }
 
-    public void getAllPostsFromGroupWithPostsComments(){
+    public List<Post> getAllPostsFromGroupWithPostsComments(int ownerId) {
 
+        List<Post> wallPosts = getAllPostsFromGroup(ownerId);
+
+        for (Post post : wallPosts) {
+            if (post.getCommentsCount() != 0) {
+                int remains = post.getCommentsCount();
+                List<Comment> postComments = new ArrayList<>();
+
+                int iteration = 0;
+                int offset;
+                do {
+                    offset = iteration * COMMENTS_FETCH_SIZE;
+                    int fetchSize = Math.min(remains, COMMENTS_FETCH_SIZE);
+                    List<Comment> subComments =
+                            wallService.getPostComments(ownerId, post.getId(), true, offset, fetchSize);
+                    postComments.addAll(subComments);
+                    remains -= fetchSize;
+                    iteration++;
+                } while (remains > 0);
+                post.setComments(postComments);
+            }
+        }
+        return wallPosts;
     }
 }
