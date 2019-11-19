3e18507f68decf061b09ab4b5fe425c7dbb973c9
Kirill Kuyan <progingisfun@gmail.com>
2019-11-19 18:46:45 +0300

    Updated WallService contract

--- src/main/java/com/project/shishafan/services/interfaces/WallService.java
+++ src/main/java/com/project/shishafan/services/interfaces/WallService.java
@@ -4,13 +4,18 @@ package com.project.shishafan.services.interfaces;
  *  Date: 17.11.2019
  */
 
+import com.project.shishafan.drivers.models.Comment;
+import com.project.shishafan.drivers.models.Post;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.enums.WallFilter;
 import com.vk.api.sdk.objects.wall.responses.GetCommentsResponse;
 import com.vk.api.sdk.objects.wall.responses.GetResponse;
 
+import java.util.List;
+
 public interface WallService {
 
-    GetResponse getWallPosts(int ownerId, int count, int offset, WallFilter filter);
-    GetCommentsResponse getPostComments(int ownerId, int postId, boolean likes, int offset, int count);
+    Integer getPostsCount(int ownerId, WallFilter wallFilter);
+    List<Post> getWallPosts(int ownerId, int count, int offset, WallFilter filter);
+    List<Comment> getPostComments(int ownerId, int postId, boolean likes, int offset, int count);
 }
