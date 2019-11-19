c46ac4180027deddde39792e80e4f8a3f63a3058
Kirill Kuyan <progingisfun@gmail.com>
2019-11-19 20:29:27 +0300

    Added GroupsService
    Added groups interface and implementation

---
+++ src/main/java/com/project/shishafan/services/interfaces/GroupService.java
@@ -0,0 +1,11 @@
+package com.project.shishafan.services.interfaces;
+/* Created by Kuyan Kirill
+ *  Email: progingisfun@gmail.com
+ *  Date: 19.11.2019
+ */
+
+public interface GroupService {
+
+    Integer getGroupIdByName(String name);
+
+}
