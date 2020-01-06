d361b083d3a868b1b64076f01f0d267c479d0a4e
Ilgiz Mustafin <ilgimustafin@gmail.com>
2020-01-06 08:42:42 +0300

    Replace simple end-line with space

--- data/man-ru/to_text.py
+++ data/man-ru/to_text.py
@@ -15,6 +15,7 @@ def sub(p, r, flags=re.MULTILINE):
 
 sub(r"^\.\\\".*$", '')
 sub(r"^\.\w\w.*$", '')
+sub(r"^\.\w ", '')
 sub(r"\\f.", '')
 sub(r" +", ' ')
 
@@ -22,5 +23,6 @@ text = re.sub(r'\n\s*\n', '\n\n', text)
 text = re.sub(r'^\n*', '', text)
 text = re.sub(r'\\-', '-', text)
 text = re.sub(r'\\ ', ' ', text)
+text = re.sub(r'(\w)\n(\w)', '\\1 \\2', text)
 text = '\0' + text + '\1'
 print(text)
