d4ad2c79f6444675b6a24521303e6ce047242116
Ilgiz Mustafin <ilgimustafin@gmail.com>
2020-01-06 08:28:13 +0300

    Add ru man pages

---
+++ data/man-ru/to_text.py
@@ -0,0 +1,26 @@
+import sys
+import re
+
+text = sys.stdin.read()
+
+if not text:
+    sys.exit()
+
+if text.startswith('.so'):
+    sys.exit()
+
+def sub(p, r, flags=re.MULTILINE):
+    global text
+    text = re.sub(p, r, text, flags=re.MULTILINE)
+
+sub(r"^\.\\\".*$", '')
+sub(r"^\.\w\w.*$", '')
+sub(r"\\f.", '')
+sub(r" +", ' ')
+
+text = re.sub(r'\n\s*\n', '\n\n', text)
+text = re.sub(r'^\n*', '', text)
+text = re.sub(r'\\-', '-', text)
+text = re.sub(r'\\ ', ' ', text)
+text = '\0' + text + '\1'
+print(text)
